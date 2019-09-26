46d9f1f0b2fba0f093dc74f2062a81dd71a9e964
rizki2704 <55817452+rizki2704@users.noreply.github.com>
2019-09-26 08:06:45 +0700

    Add files via upload

---
+++ src/if01/pkg10118025/latihan7/integer/IF0110118025Latihan7Integer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118025.latihan7.integer;
+
+/**
+ *
+ * @author
+ * Nama     : Rizki Restu Illahi
+ * Kelas    : IF01
+ * NIM      : 10118025
+ */
+public class IF0110118025Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        
+        System.out.println("byte : " +b);
+        System.out.println("short : " +s);
+        System.out.println("int : " +i);
+        System.out.println("long : " +l);
+    }
+    
+}
